f9ef07644cc0635692eeaf071bad0d5e766e5053
LuizHansen <luizhansen751@gmail.com>
2025-03-22 12:18:39 -0300

    feat [client]: Inserção do front-end da aplicação

--- server/main.py
+++ server/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from db import cria_tabelas
 from config.settings import HOST, PORT, RELOAD
 from routes.cnab_router import cnab_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(cnab_router)
 
